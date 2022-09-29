b32a8d4d3a1fe460a0c796f2195e506d863fb20a
Vick Scarlet <scarlet_vick@outlook.com>
2022-09-29 23:24:28 +0800

    feature: asset enable assets object

--- src/module/asset/index.js
+++ src/module/asset/index.js
@@ -3,18 +3,20 @@ import IModule from "../imodule.js";
 export default class Asset extends IModule {
 
     async rewardAssets(uid, assets) {
-        return this.$db.asset.reward(uid, assets)
-    }
-
-    async reward(uid, rewardId) {
-        const assets = this.$sheet.get('reward', rewardId, 'rewards');
-        if(!assets) return false;
-        const result = await this.rewardAssets(uid, assets);
+        const result = await this.$db.asset.reward(uid, assets)
         if(!assets.money) return result;
         // record
         const record = {c: assets.money};
         await this.$db.record.records(uid, record);
         return result;
+    }
+
+    async reward(uid, assets) {
+        if(typeof assets === "string") {
+            assets = this.$sheet.get('reward', rewardId, 'rewards');
+        }
+        if(!assets) return false;
+        return this.rewardAssets(uid, assets);
     }
 
     async check(uid, assets) {
